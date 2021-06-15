39b97bc390f3bd189dc9ddf9fa90e8981a689729
Marcos Rodrigues <marcos.rodrigues@inpe.br>
2021-06-15 09:55:32 -0300

    Changes to save merged vector file with spatial reference from original files

--- mergeShapes.py
+++ mergeShapes.py
@@ -1,5 +1,5 @@
 #Based on https://gis.stackexchange.com/a/372556
-from osgeo import ogr
+from osgeo import ogr, osr
 import os
 
 directory = "./shp/"
@@ -16,20 +16,31 @@ if os.path.exists(outputMergefn):
 
 filelist = os.listdir(directory)
 if outputFinalfn in filelist:
-  filelist.remove(outputFinalfn)
- 
-out_ds = outputdriver.CreateDataSource(outputMergefn) 
-out_layer = out_ds.CreateLayer(outputMergefn, geom_type = geomtype)
-    
-for file in filelist:
+    filelist.remove(outputFinalfn)
+
+if len(filelist) > 0:
+    file = filelist[0]
     if file.startswith(file_start) and file.endswith(file_ext):
-         print("Processing: " + file)
-         ds = ogr.Open(directory + file)
-         if ds is None:
-             print("ds is empty")
-         lyr = ds.GetLayer()
-         for feat in lyr:
-             out_feat = ogr.Feature(out_layer.GetLayerDefn())
-             out_feat.SetGeometry(feat.GetGeometryRef().Clone())
-             out_layer.CreateFeature(out_feat)
-             out_layer.SyncToDisk()
+        ds = ogr.Open(directory + file)
+        layer = ds.GetLayer()
+        # Get projection from Layer- https://pcjericks.github.io/py-gdalogr-cookbook/projection.html
+        spatialRef = layer.GetSpatialRef()
+        print('Projection:', spatialRef.ExportToWkt())
+        
+
+    # Define output:
+    out_ds = outputdriver.CreateDataSource(outputMergefn) 
+    out_layer = out_ds.CreateLayer(outputMergefn, srs = spatialRef, geom_type = geomtype)
+    
+    for file in filelist:
+        if file.startswith(file_start) and file.endswith(file_ext):
+             print("Processing: " + file)
+             ds = ogr.Open(directory + file)
+             if ds is None:
+                 print("ds is empty")
+             lyr = ds.GetLayer()
+             for feat in lyr:
+                 out_feat = ogr.Feature(out_layer.GetLayerDefn())
+                 out_feat.SetGeometry(feat.GetGeometryRef().Clone())
+                 out_layer.CreateFeature(out_feat)
+                 out_layer.SyncToDisk()
